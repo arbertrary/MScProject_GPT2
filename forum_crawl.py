a256e300e244256f6876346d120b290d7c3a4924
Armin Bernstetter <armin.bernstetter@stud-mail.uni-wuerzburg.de>
2019-05-09 17:06:28 +0200

    added notes, modified crawler

--- forum_crawl.py
+++ forum_crawl.py
@@ -11,7 +11,6 @@ import time
 # https://www.team-andro.com/sitemap-google.xml
 
 def crawl_ta():
-    # On laptop had to replace localhost with actual IP for some reason
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     db = myclient["ta_data"]
     coll = db["ta_posts"]
@@ -21,6 +20,12 @@ def crawl_ta():
     for sitemap in andro_sitemaps:
         pages = read_sitemap(sitemap)
         for i, page in enumerate(tqdm(pages)):
+            if "phpBB3" not in page:
+                continue
+
+            if i % 20 == 0:
+                time.sleep(2)
+
             metadata = get_forum_page(page)
 
             if metadata:
@@ -36,65 +41,63 @@ def read_sitemap(url):
 
 
 def get_forum_page(url):
-    if "phpBB3" in url:
-        text = requests.get(url).text
-        soup = BeautifulSoup(text, "html.parser")
+    text = requests.get(url).text
+    soup = BeautifulSoup(text, "html.parser")
 
-        m = re.search(r"-(t\d+)(-\d+)*.html", url)
+    m = re.search(r"-(t\d+)(-\d+)*.html", url)
+    if m:
+        thread_id = m.group(1)
+    else:
+        thread_id = "t0000"
+
+    posts = soup.find_all("div", id=re.compile(r"p\d+"))
+    navlinks = soup.find("ul", class_="linklist navlinks").find_all("a")
+    navlinks = [x.getText().replace(" ", "_") for x in navlinks]
+    navlinks.append(thread_id)
+    path = os.path.join("ta_text", *navlinks)
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    meta_list = []
+
+    for post in posts:
+        post_id = post.get("id")
+
+        filepath = os.path.join(path, post_id)
+        if os.path.exists(filepath):
+            continue
+
+        post_url = url + "#" + post_id
+        text = post.find("div", class_="content").getText()
+        user_block = post.find("a", href=re.compile(r"team-andro.com/my/.+-u\d+"))
+        author_string = post.find("p", class_="author").getText()
+        author_regex = r"von\s(.+)\s.\s(\d{2}\s\w{3}\s\d{4})\s(.+)\s"
+        pattern = re.compile(author_regex)
+        m = re.search(pattern, author_string)
         if m:
-            thread_id = m.group(1)
+            author_name = m.group(1)
+            post_date = m.group(2)
+            post_time = m.group(3)
         else:
-            thread_id = "t0000"
+            author_name = "John Doe"
+            post_date = "1 Jan 2000"
+            post_time = "00:00"
 
-        posts = soup.find_all("div", id=re.compile(r"p\d+"))
-        navlinks = soup.find("ul", class_="linklist navlinks").find_all("a")
-        navlinks = [x.getText().replace(" ", "_") for x in navlinks]
-        navlinks.append(thread_id)
-        path = os.path.join("ta_text", *navlinks)
-        if not os.path.exists(path):
-            os.makedirs(path)
+        if user_block:
+            user_url = user_block.get("href")
+        else:
+            user_url = "user_deleted"
 
-        meta_list = []
+        with open(filepath, "w") as post_textfile:
+            post_textfile.write(text)
 
-        for post in posts:
-            post_id = post.get("id")
+        # Available Info:
+        metadata = {"thread_url": url, "thread_id": thread_id, "post_id": post_id, "post_url": post_url,
+                    "post_date": post_date, "post_time": post_time, "author_name": author_name,
+                    "user_url": user_url, "file_path": filepath, "text": text}
+        meta_list.append(metadata)
 
-            filepath = os.path.join(path, post_id)
-            if os.path.exists(filepath):
-                continue
-
-            post_url = url + "#" + post_id
-            text = post.find("div", class_="content").getText()
-            user_block = post.find("a", href=re.compile(r"team-andro.com/my/.+-u\d+"))
-            author_string = post.find("p", class_="author").getText()
-            author_regex = r"von\s(.+)\s.\s(\d{2}\s\w{3}\s\d{4})\s(.+)\s"
-            pattern = re.compile(author_regex)
-            m = re.search(pattern, author_string)
-            if m:
-                author_name = m.group(1)
-                post_date = m.group(2)
-                post_time = m.group(3)
-            else:
-                author_name = "John Doe"
-                post_date = "1 Jan 2000"
-                post_time = "00:00"
-
-            if user_block:
-                user_url = user_block.get("href")
-            else:
-                user_url = "user_deleted"
-
-            with open(filepath, "w") as post_textfile:
-                post_textfile.write(text)
-
-            # Available Info:
-            metadata = {"thread_url": url, "thread_id": thread_id, "post_id": post_id, "post_url": post_url,
-                        "post_date": post_date, "post_time": post_time, "author_name": author_name,
-                        "user_url": user_url, "file_path": filepath, "text": text}
-            # print(metadata)
-            meta_list.append(metadata)
-
-        return meta_list
+    return meta_list
 
 
 # def read_sitemap_xml():
@@ -135,10 +138,3 @@ def get_forum_page(url):
 
 if __name__ == '__main__':
     crawl_ta()
-    # author_string = "von Jan- » 08 Dez 2006 23:48"
-    # author_regex = r"von\s(.+)\s.\s(\d{2}\s\w{3}\s\d{4})\s(.+)"
-    # pattern = re.compile(author_regex)
-    # m = re.search(pattern, author_string)
-    # print(m)
-    # for group in m.groups():
-    #     print(group)
