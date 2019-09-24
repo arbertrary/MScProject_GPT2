bc85401bcf5ab6d559d3a203c35c234684bd5234
Armin Bernstetter <studbernstetter@vingilot.informatik.uni-wuerzburg.de>
2019-09-24 14:55:32 +0200

    added allenai/bilm-tf repo as folder

---
+++ allenai/bilm-tf/predict.py
@@ -0,0 +1,91 @@
+import tensorflow as tf
+import os
+import numpy as np
+from bilm import Batcher, BidirectionalLanguageModel, weight_layers
+
+datadir = os.path.join("/home/stud/bernstetter/super_corpus/", "test")
+vocab_file = os.path.join(datadir, 'vocab_test.txt')
+options_file = os.path.join(datadir, 'options.json')
+weight_file = os.path.join(datadir, 'weights-20190705.hdf5')
+
+# Create a Batcher to map text to character ids.
+batcher = Batcher(vocab_file, 50)
+print("###############################")
+print(batcher._lm_vocab._word_char_ids[batcher._lm_vocab.bos])
+print("##############################")
+
+# Input placeholders to the biLM.
+context_character_ids = tf.placeholder('int32', shape=(None, None, 50))
+question_character_ids = tf.placeholder('int32', shape=(None, None, 50))
+
+# Build the biLM graph.
+bilm = BidirectionalLanguageModel(options_file, weight_file)
+
+# Get ops to compute the LM embeddings.
+context_embeddings_op = bilm(context_character_ids)
+question_embeddings_op = bilm(question_character_ids)
+
+# Get an op to compute ELMo (weighted average of the internal biLM layers)
+# Our SQuAD model includes ELMo at both the input and output layers
+# of the task GRU, so we need 4x ELMo representations for the question
+# and context at each of the input and output.
+# We use the same ELMo weights for both the question and context
+# at each of the input and output.
+elmo_context_input = weight_layers('input', context_embeddings_op, l2_coef=0.0)
+with tf.variable_scope('', reuse=True):
+    # the reuse=True scope reuses weights from the context for the question
+    elmo_question_input = weight_layers(
+        'input', question_embeddings_op, l2_coef=0.0
+    )
+
+elmo_context_output = weight_layers(
+    'output', context_embeddings_op, l2_coef=0.0
+)
+
+with tf.variable_scope('', reuse=True):
+    # the reuse=True scope reuses weights from the context for the question
+    elmo_question_output = weight_layers(
+        'output', question_embeddings_op, l2_coef=0.0
+    )
+
+
+# Now we can compute embeddings.
+raw_context = [
+        "Man kann mehrere Tage in der Woche trainieren.",
+        "Beispielsweise mit einem Trainingsplan für den ganzen Körper."
+]
+
+tokenized_context = [sentence.split() for sentence in raw_context]
+tokenized_question = [
+    ['Wie', 'oft', 'soll', 'ich', 'trainieren', '?'],
+]
+
+with tf.Session() as sess:
+    # It is necessary to initialize variables once before running inference.
+    sess.run(tf.global_variables_initializer())
+
+    # Create batches of data.
+    context_ids = batcher.batch_sentences(tokenized_context)
+    question_ids = batcher.batch_sentences(tokenized_question)
+    
+
+    # Compute ELMo representations (here for the input only, for simplicity).
+    elmo_context_input_, elmo_question_input_ = sess.run(
+        [elmo_context_input['weighted_op'], elmo_question_input['weighted_op']],
+        feed_dict={context_character_ids: context_ids,
+                   question_character_ids: question_ids}
+    )
+
+    elmo_context_output_, elmo_question_output_ = sess.run(
+        [elmo_context_output['weighted_op'], elmo_question_output['weighted_op']],
+        feed_dict={context_character_ids: context_ids,
+                   question_character_ids: question_ids}
+    )
+
+    print(elmo_question_input_.shape)
+    print(elmo_context_input_)
+    print(elmo_context_output_)
+
+
+
+
